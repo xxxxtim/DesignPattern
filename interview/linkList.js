cf034a7f30500a3f68820349fedd41920c196311
xxxxtim <xxxxtim@hotmail.com>
2020-06-28 23:32:43 +0800

    feat: append complete

--- interview/linkList.js
+++ interview/linkList.js
@@ -1,15 +1,39 @@
-class LInkList {
+class LinkList {
     constructor() {
         // the head of linklist
         this.head = null;
         // the length of linklist
         this.length = 0;
     }
-    node(element) {
-        this.element = element;
-        this.next = null
+    append(item) {
+        let node = new Node(item);
+        // case1 
+        if (this.head == null) {
+            this.head = node;
+            console.table(this.head);
+        }
+        else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            // while 執行完之後 current會到鏈表尾
+            current.next = node
+            console.table(this.head);
+        }
+        this.length++;
     }
 
 
-
 }
+
+class Node {
+    constructor(item) {
+        this.item = item;
+        this.next = null;
+    }
+}
+let link = new LinkList;
+link.append('yachen');
+link.append('titan');
+link.append('xxxtim');
